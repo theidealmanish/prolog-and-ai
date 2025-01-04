f588900f01de29da175c3e72406832e85bc43023
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2025-01-04 22:57:39 +0545

    added basic facts

---
+++ 1.basic_facts.pl
@@ -0,0 +1,16 @@
+% facts => knowledge base
+% likes(john, mary). => john likes mary => obj1 is john, obj2 is mary
+% relation should start with a lowercase letter
+
+likes(john, mary).
+valuable(gold).
+father(john, mary).
+owns(john, gold).
+gives(john, book, mary).
+plays(john, mary, football).
+
+% here, john gives book to mary
+% john plays football with mary
+% the formats are different, but the meaning is the same
+
+
