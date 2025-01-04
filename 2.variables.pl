67a4b22c0bc807db61b429c8d70f69b89f49085c
Manish Shivabhakti <manish.shivabhakti.99@gmail.com>
2025-01-04 22:57:49 +0545

    added basic variables

---
+++ 2.variables.pl
@@ -0,0 +1,12 @@
+likes(john, flowers).
+likes(john, chicks).
+likes(john, mary).
+likes(john, mangoes).
+dislikes(john, snakes).
+
+% john likes flowers, chicks, mary, mangoes
+% john dislikes snakes
+% to get the list of things john likes, we can use the query:
+% likes(john, X). [Remember capital X, even Mary is a variable]
+% this will return all the things john likes
+% use semi-colon to get the next result
